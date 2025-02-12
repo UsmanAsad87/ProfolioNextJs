1593dfe82360e5470a3511ff156d370ab8e81ce0
UsmanAsad87 <usmanasad0324@gmail.com>
2025-02-12 20:33:39 +0500

    code fixed

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -7,7 +7,7 @@ import Lottie from "react-lottie";
 import { cn } from "@/lib/utils";
 
 
-import { BackgroundGradientAnimation } from "./GradientBg";
+// import { BackgroundGradientAnimation } from "./GradientBg";
 import GridGlobe from "./GridGlobe";
 import animationData from "@/data/confetti.json";
 import MagicButton from "../MagicButton";
@@ -109,9 +109,9 @@ export const BentoGridItem = ({
           )}
         </div>
         {id === 6 && (
-          <BackgroundGradientAnimation>
+          // <BackgroundGradientAnimation>
             <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
-          </BackgroundGradientAnimation>
+          {/* </BackgroundGradientAnimation> */}
         )}
 
         <div
