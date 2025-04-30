6e6a80ea2d77a8d4a702ee872eaa4f33763c71ff
UsmanAsad87 <usmanasad0324@gmail.com>
2025-04-30 17:35:50 +0500

    Google site verification added and chat bot

--- app/page.tsx
+++ app/page.tsx
@@ -16,10 +16,7 @@ import Head from 'next/head';
 const Home = () => {
   return (
     <>
-      <Head>
-        <meta name="google-site-verification" content="ggWKJgOz1lENFu8n__fDmACwmEPdtGkYRaUOgDXjpvQ" />
-        <script src="https://4ai.chat/embed.js?siteId=NEFJQ0hBVEtFWTEwNQ==&app=web"></script>
-      </Head>
+     
       <SEO title="Usman Asad - Digifyr Portfolio" description="Explore the portfolio of Usman Asad, a professional at Digifyr and NESCOM. Discover projects, experience, and more." />
       <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
         <div className="max-w-7xl w-full">
@@ -32,7 +29,6 @@ const Home = () => {
           <Approach/>
           <Footer/>
         </div>
-        <script src="https://4ai.chat/embed.js?siteId=NEFJQ0hBVEtFWTEwNQ==&app=web"></script>
       </main>
     </>
   );
